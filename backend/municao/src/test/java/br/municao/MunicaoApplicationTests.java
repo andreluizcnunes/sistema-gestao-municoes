d18d1965d0a63639d613b030bb3d0d448b177554
André Nunes <andre.doctors@gmail.com>
2023-09-05 20:48:23 -0300

    adicionando estrutura do backend

---
+++ backend/municao/src/test/java/br/municao/MunicaoApplicationTests.java
@@ -0,0 +1,13 @@
+package br.municao;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MunicaoApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
