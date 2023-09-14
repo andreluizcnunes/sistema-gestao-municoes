3b979773f803bb383ccefe9ee402298ab59e0f79
André Nunes <andre.doctors@gmail.com>
2023-09-14 20:58:18 -0300

    created component HeaderPolicial

---
+++ frontend/gestaoMunicao/src/pages/PainelPolicial/components/HeaderPolicial/index.jsx
@@ -0,0 +1,24 @@
+import { ContentInfo, ContainerBtn } from "./HeaderPolicial.styled"
+
+import BtnMenu from "../../../../components/BtnMenu";
+
+import IconAdd from "../../../../assets/icons/iconAdd.png";
+import IconBusca from "../../../../assets/icons/iconBusca.png";
+import IconHome from "../../../../assets/icons/iconHome.png";
+
+function HeaderPolicial({ titulo }){
+    return(
+        <ContentInfo>
+            <h2>{titulo}</h2>
+            <hr />
+            <ContainerBtn>
+                <BtnMenu linkBotao={"/policial"} iconBotao={IconHome} textBotao={"Inicio"} />
+                <BtnMenu linkBotao={"/policial/cadastro"} iconBotao={IconAdd} textBotao={"Cadastrar"} />
+                <BtnMenu linkBotao={"/policial/listar"} iconBotao={IconBusca} textBotao={"Consultar"} />
+            </ContainerBtn>
+
+        </ContentInfo>
+    );
+}
+
+export default HeaderPolicial;
