c7e345f52b9bae3ec7a2560e516019bc20c11308
André Nunes <andre.doctors@gmail.com>
2023-09-14 16:12:45 -0300

    Estilizacao painel editar municao desktop 100%

--- frontend/gestaoMunicao/src/pages/PainelMunicaoEditar/index.jsx
+++ frontend/gestaoMunicao/src/pages/PainelMunicaoEditar/index.jsx
@@ -1,10 +1,22 @@
+import { CMain, ContentMain, CSection } from "./MunicaoEditar.styled"
+
+import TopBar from "../../components/TopBar";
+import LeftBar from "../../components/LerftBar"
+import HeaderMunicao from "../PainelMunicao/components/HeaderMunicao";
+import FormMunicao from "../PainelMunicao/components/FormMunicao";
+
 function PainelMunicaoEditar(){
     return(
-        <>
-            <h1>
-                Painel Editar Munição
-            </h1>
-        </>
+        <CMain>
+            <TopBar/>
+            <ContentMain>
+                <LeftBar/>
+                <CSection>
+                    <HeaderMunicao  titulo={"Editar Munição"} />
+                    <FormMunicao/>
+                </CSection>
+            </ContentMain>
+        </CMain>
     );
 }
 
