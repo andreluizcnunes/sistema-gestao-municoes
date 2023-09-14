f9e368755f9d9ca0f966688ea454fd797fb90a3f
André Nunes <andre.doctors@gmail.com>
2023-09-14 16:11:44 -0300

    Estilizacao do Painel Municao Cadastro desktop 100%

--- frontend/gestaoMunicao/src/pages/PainelMunicaoCadastrar/index.jsx
+++ frontend/gestaoMunicao/src/pages/PainelMunicaoCadastrar/index.jsx
@@ -1,10 +1,22 @@
+import { CMain, ContentMain, CSection } from "./MunicaoCadastrar.styled"
+
+import TopBar from "../../components/TopBar";
+import LeftBar from "../../components/LerftBar"
+import HeaderMunicao from "../PainelMunicao/components/HeaderMunicao";
+import FormMunicao from "../PainelMunicao/components/FormMunicao";
+
 function PainelMunicaoCadastrar(){
     return(
-        <>
-            <h1>
-                Painel Cadastrar Munição
-            </h1>
-        </>
+        <CMain>
+            <TopBar/>
+            <ContentMain>
+                <LeftBar/>
+                <CSection>
+                    <HeaderMunicao  titulo={"Cadastrar Munição"} />
+                    <FormMunicao/>
+                </CSection>
+            </ContentMain>
+        </CMain>
     );
 }
 
