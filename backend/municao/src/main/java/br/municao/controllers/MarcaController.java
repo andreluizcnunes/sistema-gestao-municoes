6cf135efe0bff1b34a8f8c6150b57054a8c8cc34
André Nunes <andre.doctors@gmail.com>
2023-09-15 09:48:13 -0300

    permissao de rota do frontend

--- backend/municao/src/main/java/br/municao/controllers/MarcaController.java
+++ backend/municao/src/main/java/br/municao/controllers/MarcaController.java
@@ -14,6 +14,7 @@ import java.util.List;
  */
 @RestController
 @RequestMapping("/marca")
+@CrossOrigin(origins = "http://localhost:5173")
 public class MarcaController {
     private final MarcaService marcaService;
 
