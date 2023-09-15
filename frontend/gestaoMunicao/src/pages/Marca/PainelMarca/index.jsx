21f859221cd601e106414facc64b2ed0c111a269
André Nunes <andre.doctors@gmail.com>
2023-09-15 16:21:40 -0300

    feat: Adiciona exibição de marcas na página inicial
    - Inicializa o estado para armazenar informações sobre as marcas.
    - Utiliza o useEffect para buscar as marcas da API local em http://localhost:8080/marca/listar e atualizar o estado.
    - Define um objeto padrão marca com propriedades id e nome.
    - Exibe as marcas em uma tabela na página inicial.
    
    Isso fecha #1 (se houver uma issue relacionada)

--- frontend/gestaoMunicao/src/pages/Marca/PainelMarca/index.jsx
+++ frontend/gestaoMunicao/src/pages/Marca/PainelMarca/index.jsx
@@ -1,16 +1,38 @@
-import { CMain, ContentMain } from "./Marca.styled"
+import { CMain, ContentMain, CSection } from "./Marca.styled"
 
 import TopBar from "../../../components/TopBar";
 import LeftBar from "../../../components/LerftBar"
 import HeaderMarca from "./components/HeaderMarca";
+import { useEffect, useState } from "react";
+import TabelaMarca from "./components/TabelaMarca";
 
 function PainelMarca(){
+
+    const marca = {
+        id: 0,
+        nome: ''
+    }
+
+    const [marcas, setMarcas] = useState([]);
+    const [objMarca, setObjMarca] = useState(marca);
+
+    useEffect(() => {
+        fetch("http://localhost:8080/marca/listar")
+        .then(retorno => retorno.json())
+        .then(retornoConvertido => setMarcas(retornoConvertido));
+    }, []);
+
     return(
         <CMain>
             <TopBar/>
             <ContentMain>
                 <LeftBar/>
-                <HeaderMarca  titulo={"Painel Marca"} />
+                <CSection>
+                    <HeaderMarca  titulo={"Painel Marca"} />
+                    <TabelaMarca 
+                        vetor={marcas}
+                    />
+                </CSection>
             </ContentMain>
         </CMain>
     );
