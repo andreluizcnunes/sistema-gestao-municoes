19430aef31ee69f78f013061e0c1b51bb4de32ff
André Nunes <andre.doctors@gmail.com>
2023-09-14 23:35:11 -0300

    Estilização da pagina Policial Listar

--- frontend/gestaoMunicao/src/pages/PainelPolicialLista/index.jsx
+++ frontend/gestaoMunicao/src/pages/PainelPolicialLista/index.jsx
@@ -1,10 +1,22 @@
+import { CMain, ContentMain, CSection } from "./PolicialEditar.styled"
+
+import TopBar from "../../components/TopBar";
+import LeftBar from "../../components/LerftBar"
+import HeaderPolicial from "../PainelPolicial/components/HeaderPolicial";
+import BuscarPolicial from "../PainelPolicial/components/BuscarPolicial";
+
 function PainelPolicialLista(){
     return(
-        <>
-            <h1>
-                Painel Lista Policial
-            </h1>
-        </>
+        <CMain>
+            <TopBar/>
+            <ContentMain>
+                <LeftBar/>
+                <CSection>
+                    <HeaderPolicial  titulo={"Pesquisar Policial"} />
+                    <BuscarPolicial/>
+                </CSection>
+            </ContentMain>
+        </CMain>
     );
 }
 
