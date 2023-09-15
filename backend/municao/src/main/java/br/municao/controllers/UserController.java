850db29fd47653b2fe5bbb4cd70fa22f6e45da43
André Nunes <andre.doctors@gmail.com>
2023-09-15 13:35:24 -0300

    Configura permissões CORS para a origem http://localhost:5173
    Neste commit, adicionei a anotação @CrossOrigin no controlador para permitir solicitações vindas da origem http://localhost:5173. Isso facilita o desenvolvimento e o teste da aplicação ao permitir solicitações cruzadas a partir deste domínio específico.

--- backend/municao/src/main/java/br/municao/controllers/UserController.java
+++ backend/municao/src/main/java/br/municao/controllers/UserController.java
@@ -11,6 +11,7 @@ import java.util.List;
 
 @RestController
 @RequestMapping("/users")
+@CrossOrigin(origins = "http://localhost:5173")
 public class UserController {
 
     private final UserService userService;
