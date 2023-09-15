b3b972f61e37dc57fe9d66844a17d5f379630c9b
André Nunes <andre.doctors@gmail.com>
2023-09-15 12:06:01 -0300

    Implementa funcionalidade de cadastro de fornecedor
    Neste commit, adicionei a funcionalidade de cadastro de fornecedor no componente. Foram adicionados estados para armazenar os dados do fornecedor, uma função para atualizar esses estados com base nos dados do formulário e uma função para enviar uma solicitação POST para cadastrar o fornecedor no servidor. Também foi incluída a funcionalidade de limpar o formulário após o cadastro bem-sucedido.

--- frontend/gestaoMunicao/src/pages/Fornecedor/PainelFornecedor/components/FormFornecedor/index.jsx
+++ frontend/gestaoMunicao/src/pages/Fornecedor/PainelFornecedor/components/FormFornecedor/index.jsx
@@ -1,13 +1,21 @@
 import { Cmain, RazaoSocial, Cnpj, Inscricao, GroupInput } from './FormFornecedor.styled';
 
-function FormFornecedor(){
+function FormFornecedor({ getDadosForm, obj, cadastrar }){
     return(
         <Cmain>
             <RazaoSocial htmlFor="razaoSocial">
                 <p>
                     Razão Social: <span>(Campo Obrigatório)</span>
                 </p>
-                <input type="text" name='razaoSocial' id='razaoSocial' placeholder='Digite a Razão Social' required/>
+                <input 
+                    type="text" 
+                    name='razaoSocial' 
+                    id='razaoSocial' 
+                    placeholder='Digite a Razão Social'
+                    onChange={getDadosForm} 
+                    value={obj.razaoSocial}
+                    required
+                />
             </RazaoSocial>
 
             <GroupInput>
@@ -15,18 +23,33 @@ function FormFornecedor(){
                     <p>
                         CNPJ: <span>(Campo Obrigatório)</span>
                     </p>
-                    <input type="text" name='cnpj' id='cnpj' placeholder='Digite o CNPJ' required/>
+                    <input 
+                        type="text" 
+                        name='cnpj' 
+                        id='cnpj' 
+                        placeholder='Digite o CNPJ' 
+                        onChange={getDadosForm} 
+                        value={obj.cnpj}
+                        required
+                    />
                 </Cnpj>
 
-                <Inscricao htmlFor="razaoSocial">
+                <Inscricao htmlFor="inscricao">
                     <p>
                         Inscrição Estadual:
                     </p>
-                    <input type="text" name='razaoSocial' id='razaoSocial' placeholder='Digite a Razão Social'/>
+                    <input 
+                        type="text" 
+                        name='inscricao' 
+                        id='inscricao' 
+                        placeholder='Digite a Razão Social'
+                        onChange={getDadosForm} 
+                        value={obj.inscricao}
+                    />
                 </Inscricao>
             </GroupInput>            
 
-            <button type='submit'>
+            <button onClick={cadastrar} type='submit'>
                 Salvar
             </button>
         </Cmain>
--- frontend/gestaoMunicao/src/pages/Fornecedor/PainelFornecedorCadastro/index.jsx
+++ frontend/gestaoMunicao/src/pages/Fornecedor/PainelFornecedorCadastro/index.jsx
@@ -5,8 +5,49 @@ import TopBar from "../../../components/TopBar";
 
 import HeaderFornecedor from "../PainelFornecedor/components/HeaderFornecedor";
 import FormFornecedor from "../PainelFornecedor/components/FormFornecedor";
+import { useState } from "react";
 
 function PainelFornecedorCadastro(){
+
+    const fornecedor = {
+        razaoSocial: '',
+        cnpj: '',
+        inscricao: ''
+    }
+
+    const [fornecedores, setFornecedores] = useState([]);
+    const [objFornecedor, setObjFornecedor] = useState(fornecedor);
+
+    const getInputForm = (e) => {
+        setObjFornecedor({ ...objFornecedor, [e.target.name]:e.target.value });
+    }
+
+    const cadastrarFornecedor = () => {
+        fetch("http://localhost:8080/fornecedor/cadastrar", {
+            method: 'post',
+            body: JSON.stringify(objFornecedor),
+            headers:{
+                'Content-type': 'application/json',
+                'Accept': 'application/json'
+            }
+        })
+
+        .then(retorno => retorno.json())
+        .then(retorno_convertido =>{
+            if(retorno_convertido.mensagem != undefined){
+                alert(retorno_convertido.mensagem);
+            }else{
+                setFornecedores([ ...fornecedores, retorno_convertido]);
+                alert("Fornecedor cadastrado com Sucesso!");
+                limparFormulario();
+            }
+        })
+    }
+
+    const limparFormulario = () =>{
+        setObjFornecedor(fornecedor);
+    }
+
     return(
         <CMain>
             <TopBar/>
@@ -14,7 +55,7 @@ function PainelFornecedorCadastro(){
                 <LeftBar/>
                 <CSection>
                     <HeaderFornecedor titulo={"Cadastro de Fornecedor"}/>
-                    <FormFornecedor/>
+                    <FormFornecedor getDadosForm={getInputForm} obj={objFornecedor} cadastrar={cadastrarFornecedor}/>
                 </CSection>
             </ContentMain>
         </CMain>
