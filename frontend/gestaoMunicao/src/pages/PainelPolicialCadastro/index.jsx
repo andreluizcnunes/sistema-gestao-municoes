7779207eddb817680c81e948fe102cc467b2f301
André Nunes <andre.doctors@gmail.com>
2023-09-14 22:50:21 -0300

    estilizando painel cadastro Polical

--- frontend/gestaoMunicao/src/pages/PainelPolicialCadastro/index.jsx
+++ frontend/gestaoMunicao/src/pages/PainelPolicialCadastro/index.jsx
@@ -1,10 +1,22 @@
+import { CMain, ContentMain, CSection } from "./PolicialCadastrar.styled"
+
+import TopBar from "../../components/TopBar";
+import LeftBar from "../../components/LerftBar"
+import HeaderPolicial from "../PainelPolicial/components/HeaderPolicial";
+import FormPolicial from "../PainelPolicial/components/FormPolicial";
+
 function PainelPolicialCadastro(){
     return(
-        <>
-            <h1>
-                Painel Cadastro Policial
-            </h1>
-        </>
+        <CMain>
+            <TopBar/>
+            <ContentMain>
+                <LeftBar/>
+                <CSection>
+                    <HeaderPolicial  titulo={"Cadastrar Policial"} />
+                    <FormPolicial/>
+                </CSection>
+            </ContentMain>
+        </CMain>
     );
 }
 
