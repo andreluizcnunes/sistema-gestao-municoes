e075d04535452bd134914d3ddc8d3beeae6ac302
André Nunes <andre.doctors@gmail.com>
2023-09-14 23:36:08 -0300

    Estilização da pagina editar policial

--- frontend/gestaoMunicao/src/pages/PainelPolicialEditar/index.jsx
+++ frontend/gestaoMunicao/src/pages/PainelPolicialEditar/index.jsx
@@ -1,10 +1,22 @@
+import { CMain, ContentMain, CSection } from "./PolicialEditar"
+
+import TopBar from "../../components/TopBar";
+import LeftBar from "../../components/LerftBar"
+import HeaderPolicial from "../PainelPolicial/components/HeaderPolicial";
+import FormPolicial from "../PainelPolicial/components/FormPolicial";
+
 function PainelPolicialEditar(){
     return(
-        <>
-            <h1>
-                Painel Editar Policial
-            </h1>
-        </>
+        <CMain>
+            <TopBar/>
+            <ContentMain>
+                <LeftBar/>
+                <CSection>
+                    <HeaderPolicial  titulo={"Editar Policial"} />
+                    <FormPolicial/>
+                </CSection>
+            </ContentMain>
+        </CMain>
     );
 }
 
