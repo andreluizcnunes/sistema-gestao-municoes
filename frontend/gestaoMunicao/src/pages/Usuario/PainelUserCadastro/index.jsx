1e5d0c320519559ecc76a6dd37232a2d4f6c2554
André Nunes <andre.doctors@gmail.com>
2023-09-15 00:46:10 -0300

    Estilização page Usuario Cadastro

--- frontend/gestaoMunicao/src/pages/Usuario/PainelUserCadastro/index.jsx
+++ frontend/gestaoMunicao/src/pages/Usuario/PainelUserCadastro/index.jsx
@@ -1,10 +1,23 @@
+import { CMain, ContentMain, CSection } from "./UserCadastro.styled"
+
+
+import TopBar from "../../../components/TopBar";
+import LeftBar from "../../../components/LerftBar"
+import HeaderUser from "../PainelUser/components/HeaderUser";
+import FormUsuario from "../PainelUser/components/FormUsuario";
+
 function PainelUserCadastro(){
     return(
-        <>
-            <h1>
-                Painel Cadastro de Usuario do Sistema
-            </h1>
-        </>
+        <CMain>
+            <TopBar/>
+            <ContentMain>
+                <LeftBar/>
+                <CSection>
+                    <HeaderUser  titulo={"Cadastrar Usuário"} />
+                    <FormUsuario/>
+                </CSection>
+            </ContentMain>
+        </CMain>
     );
 }
 
