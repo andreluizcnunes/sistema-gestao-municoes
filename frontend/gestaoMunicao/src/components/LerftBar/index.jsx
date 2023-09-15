f09311cf61e77baaf043bb214280338a94fe34bf
André Nunes <andre.doctors@gmail.com>
2023-09-15 00:09:28 -0300

    adicionando o botao inicio

--- frontend/gestaoMunicao/src/components/LerftBar/index.jsx
+++ frontend/gestaoMunicao/src/components/LerftBar/index.jsx
@@ -3,6 +3,7 @@ import { CLeftBar } from "./LeftBar.styled";
 function LeftBar(){
     return(
         <CLeftBar>
+            <a href="/dashboard">Inicio</a>
             <a href="/dashboard">Lançamentos</a>
             <a href="/municao">Munições</a>
             <a href="/policial">Policiais</a>
