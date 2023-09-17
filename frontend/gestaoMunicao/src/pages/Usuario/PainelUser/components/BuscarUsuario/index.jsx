d6bc40cbf33e5606364335b6da468971561ae99e
André Nunes <andre.doctors@gmail.com>
2023-09-17 20:29:02 -0300

    Feat: Frontend adicionei a funcao de buscar por nome

--- frontend/gestaoMunicao/src/pages/Usuario/PainelUser/components/BuscarUsuario/index.jsx
+++ frontend/gestaoMunicao/src/pages/Usuario/PainelUser/components/BuscarUsuario/index.jsx
@@ -1,9 +1,33 @@
 import { Cmain, BuscarUsuario } from "./BuscarUsuario.styled"
 
+import TabelaUsuario from "../TabelaUsuario";
+import { useEffect, useState } from "react";
+
 function BucarUsuario(){
+
+    const [usuario, setUsuario] = useState('')
+    const [resultados, setResultados] = useState([]);
+
+    useEffect(() => {
+        if (usuario) {
+            fetch(`http://localhost:8080/users/consultar?nome=${usuario}`)
+              .then(retorno => retorno.json())
+              .then(retornoConvertido => setResultados(retornoConvertido));
+        }else {
+            // Se razaoSocial for uma string vazia, limpe os resultados
+            setResultados([]);
+        }
+
+    }, [usuario]);
+
+    const handleSubmit = (e) =>{
+        e.preventDefault();
+    }
+
+
     return(
       <Cmain>
-            <BuscarUsuario htmlFor="buscarMunicao">
+            <BuscarUsuario onSubmit={handleSubmit}>
                 <p>
                     Busca:
                 </p>
@@ -12,13 +36,21 @@ function BucarUsuario(){
                     name='buscarMunicao' 
                     id='buscarMunicao' 
                     placeholder='Digite o nome do Usuário' 
+                    value={usuario} 
+                    onChange={(e) => setUsuario(e.target.value)}
                     required
                 />
 
-                <button type='submit'>
+                {/* <button type='submit'>
                     Buscar
-                </button>
-            </BuscarUsuario>       
+                </button> */}
+            </BuscarUsuario>
+
+            <div>
+                {resultados.length > 0 ? (
+                    <TabelaUsuario vetor={resultados}/>
+                ): null}
+            </div>       
         </Cmain>
     );
 }
