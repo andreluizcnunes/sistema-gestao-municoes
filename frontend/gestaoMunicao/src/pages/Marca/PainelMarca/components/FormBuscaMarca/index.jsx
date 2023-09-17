f1e2b2748238f4cfbeebe386168f764804a2c1fb
André Nunes <andre.doctors@gmail.com>
2023-09-17 12:39:02 -0300

    feat: adicionando a função de pesquisar ao digitar

--- frontend/gestaoMunicao/src/pages/Marca/PainelMarca/components/FormBuscaMarca/index.jsx
+++ frontend/gestaoMunicao/src/pages/Marca/PainelMarca/components/FormBuscaMarca/index.jsx
@@ -1,18 +1,56 @@
+import { useEffect, useState } from "react";
+
 import { Cmain, BuscarMarca } from "./FormBuscaMarca.styled"
 
+import TabelaMacar from "../TabelaMarca"
+
 function FormBuscaMarca(){
+
+    const [marca, setMarca] = useState('')
+    const [resultados, setResultados] = useState([]);
+
+    useEffect(() => {
+        if (marca) {
+            fetch(`http://localhost:8080/marca/consultar?nome=${marca}`)
+              .then(retorno => retorno.json())
+              .then(retornoConvertido => setResultados(retornoConvertido));
+        }else {
+            // Se razaoSocial for uma string vazia, limpe os resultados
+            setResultados([]);
+        }
+
+    }, [marca]);
+
+    const handleSubmit = (e) =>{
+        e.preventDefault();
+    }
+
     return(
         <Cmain>
-            <BuscarMarca htmlFor="nomeMarca">
+            <BuscarMarca onSubmit={handleSubmit}>
                 <p>
                     Busca:
                 </p>
-                <input type="text" name='nomeMarca' id='nomeMarca' placeholder='Digite o nome da Marca' required/>
+                <input 
+                    type="text" 
+                    name='nome' 
+                    id='nome' 
+                    placeholder='Digite o nome da Marca'
+                    value={marca} 
+                    onChange={(e) => setMarca(e.target.value)}
+                    required
+                />
 
-                <button type='submit'>
+                {/* <button type='submit'>
                     Buscar
-                </button>
-            </BuscarMarca>       
+                </button> */}
+            </BuscarMarca>
+
+            <div>
+                {resultados.length > 0 ? (
+                    <TabelaMacar vetor={resultados}/>
+                ): null}
+            </div>       
         </Cmain>
     );
 }
