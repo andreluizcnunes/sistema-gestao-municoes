21120960751cad9b02e965ff6db35087572b2f1f
André Nunes <andre.doctors@gmail.com>
2023-09-19 15:51:35 -0300

    Feat: Frontend adicionei a funcao de remover marca por Id

--- frontend/gestaoMunicao/src/pages/Marca/PainelMarca/components/TabelaMarca/index.jsx
+++ frontend/gestaoMunicao/src/pages/Marca/PainelMarca/components/TabelaMarca/index.jsx
@@ -1,6 +1,23 @@
 import { CTabela } from "./TabelaMarca.styled"
 
 function TabelaMarca({ vetor }) {
+
+    const remover = (indice) => {
+        fetch(`http://localhost:8080/marca/deletar/${indice}`, {
+            method: 'delete',
+            headers: {
+                'Content-type': 'application/json',
+                'Accept': 'application/json'
+            }
+        })
+            .then(retorno => retorno.json())
+            .then(retorno_convertido => {
+
+                // mensagem de sucesso
+                alert("Removido com sucesso!");
+            })
+    }
+
     return (
         <CTabela>
             <thead>
@@ -17,7 +34,7 @@ function TabelaMarca({ vetor }) {
             </thead>
             <tbody>
                 {
-                    vetor.slice().reverse().slice(0, 6).map((obj, indice) => (
+                    vetor.map((obj, indice) => (
                         <tr key={indice}>
                             <td>{indice + 1}</td>
                             <td>{obj.nome}</td>
@@ -25,7 +42,7 @@ function TabelaMarca({ vetor }) {
                                 <button>
                                     Editar
                                 </button>
-                                <button>
+                                <button onClick={() => { remover(obj.id) }}>
                                     Deletar
                                 </button>
                             </td>
