5c71cdd8ebd19a1b3167799438ec5a06d7682375
André Nunes <andre.doctors@gmail.com>
2023-09-22 17:27:51 -0300

    Edit: Backend - Ajustei a estratégia de geração de chave primária na entidade MunicaoModel de GenerationType.AUTO para GenerationType.IDENTITY.

--- backend/municao/src/main/java/br/municao/models/MunicaoModel.java
+++ backend/municao/src/main/java/br/municao/models/MunicaoModel.java
@@ -13,7 +13,7 @@ import java.util.List;
 public class MunicaoModel {
 
     @Id
-    @GeneratedValue(strategy = GenerationType.AUTO)
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
     @Column(name = "id_municao")
     private long id;
 
