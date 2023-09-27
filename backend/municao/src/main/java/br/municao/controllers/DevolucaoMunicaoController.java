7f6b52a56e38d1c7c804c9b046e6840932aa255a
André Nunes <andre.doctors@gmail.com>
2023-09-27 04:38:09 -0300

    Feat: Backend adicionei a funcao de devolucao de municao

--- backend/municao/src/main/java/br/municao/controllers/DevolucaoMunicaoController.java
+++ backend/municao/src/main/java/br/municao/controllers/DevolucaoMunicaoController.java
@@ -1,6 +1,12 @@
 package br.municao.controllers;
 
+import br.municao.models.DevolucaoMunicaoPolicalModel;
+import br.municao.models.EntradaMunicaoModel;
 import br.municao.services.DevolucaoMunicaoService;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
@@ -12,5 +18,15 @@ public class DevolucaoMunicaoController {
 
     public DevolucaoMunicaoController(DevolucaoMunicaoService devolucaoMunicaoService){
         this.devolucaoMunicaoService = devolucaoMunicaoService;
+    }
+
+    @PostMapping("/devolucao")
+    public ResponseEntity<String> devolucao(@RequestBody DevolucaoMunicaoPolicalModel devolucao) {
+        try {
+            devolucaoMunicaoService.devolucao(devolucao);
+            return new ResponseEntity<>("Estoque de munição atualizado com sucesso", HttpStatus.OK);
+        } catch (Exception e) {
+            return new ResponseEntity<>("Erro ao atualizar o estoque de munição: " + e.getMessage(), HttpStatus.INTERNAL_SERVER_ERROR);
+        }
     }
 }
--- backend/municao/src/main/java/br/municao/services/DevolucaoMunicaoService.java
+++ backend/municao/src/main/java/br/municao/services/DevolucaoMunicaoService.java
@@ -1,18 +1,42 @@
 package br.municao.services;
 
+import br.municao.models.DevolucaoMunicaoPolicalModel;
+import br.municao.models.EntradaMunicaoModel;
+import br.municao.models.MunicaoModel;
 import br.municao.repositories.DevolucaoMunicaoRepository;
+import br.municao.repositories.MunicaoRepository;
 import br.municao.response.SmsResponse;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
 import org.springframework.stereotype.Service;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
 
 @Service
 public class DevolucaoMunicaoService {
 
     private final DevolucaoMunicaoRepository devolucaoMunicaoRepository;
 
+    private final MunicaoRepository municaoRepository;
+
     private final SmsResponse smsResponse;
 
-    public DevolucaoMunicaoService(DevolucaoMunicaoRepository devolucaoMunicaoRepository){
+    public DevolucaoMunicaoService(DevolucaoMunicaoRepository devolucaoMunicaoRepository, MunicaoRepository municaoRepository){
         this.devolucaoMunicaoRepository = devolucaoMunicaoRepository;
+        this.municaoRepository = municaoRepository;
         this.smsResponse = new SmsResponse();
+    }
+
+    public void devolucao(DevolucaoMunicaoPolicalModel devolucao){
+        // Supondo que você tenha um critério para identificar a munição, como um ID único.
+        MunicaoModel municao = municaoRepository.findById(devolucao.getMunicao().getId()).orElse(new MunicaoModel()); // Obtém o objeto MunicaoModel existente ou cria um novo.
+
+        long quantidadeAtual = municao.getQuantidade();
+        long quantidadeNova = devolucao.getQuantidade();
+
+        long total = quantidadeAtual + quantidadeNova;
+
+        municao.setQuantidade(total);
+        devolucaoMunicaoRepository.save(devolucao);
     }
 }
