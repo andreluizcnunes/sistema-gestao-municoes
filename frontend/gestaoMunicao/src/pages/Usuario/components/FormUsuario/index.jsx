57c98bc017fe8f665eeaefc37dd2a7d994fefedb
André Nunes <andre.doctors@gmail.com>
2023-09-27 13:09:59 -0300

    Frontend: Refatoração do componente FormUsuario para adicionar validação de formulário
    - Adicionou validação de email
    - Atualizou o comportamento do botão Salvar com base na validação do formulário
    - Adicionou função handleSubmit para lidar com a submissão do formulário
    - Atualizou o código para evitar a submissão padrão do formulário

--- frontend/gestaoMunicao/src/pages/Usuario/components/FormUsuario/index.jsx
+++ frontend/gestaoMunicao/src/pages/Usuario/components/FormUsuario/index.jsx
@@ -1,19 +1,69 @@
-import { Cmain, NomeCompleto, EmailUser, SenhaUser } from './FormUsuario.styled';
+import { useState } from 'react';
+import { Cmain, NomeCompleto, EmailUser, SenhaUser, ButtonBlock } from './FormUsuario.styled';
 
-function FormUsuario({ getDadosForm, cadastrar, obj }){
-    return(
+function FormUsuario({ getDadosForm, cadastrar, obj }) {
+
+    const [isFormValid, setIsFormValid] = useState(false);
+
+    const isEmailValid = (email) => {
+        // Expressão regular para validar o formato de um email
+        const emailRegex = /^[A-Za-z0-9._%+-]+@[A-Za-z0-9.-]+\.[A-Za-z]{2,}$/;
+        return emailRegex.test(email);
+    };
+
+    const handleSubmit = (e) => {
+        e.preventDefault(); // Evite a submissão padrão do formulário
+        if (isFormValid) {
+            cadastrar(); // Chame a função de cadastro;
+            window.location.reload();
+        }
+    };
+
+    const handleInputChange = (e) => {
+        getDadosForm(e);
+
+        // Valide os campos aqui e atualize o estado isFormValid
+        const { name, value } = e.target;
+
+        if (
+            name === 'nome' &&
+            value.trim() !== '' &&
+            isEmailValid(obj.email) && // Verificar se o email é válido
+            obj.senha !== ''
+        ) {
+            setIsFormValid(true);
+        } else if (
+            name === 'email' &&
+            obj.nome !== '' &&
+            value.trim() !== '' &&
+            isEmailValid(value) // Verificar se o email é válido
+        ) {
+            setIsFormValid(true);
+        } else if (
+            name === 'senha' &&
+            obj.nome !== '' &&
+            isEmailValid(obj.email) && // Verificar se o email é válido
+            value.trim() !== ''
+        ) {
+            setIsFormValid(true);
+        } else {
+            setIsFormValid(false);
+        }
+    };
+
+    return (
         <Cmain>
             <NomeCompleto htmlFor="nome">
                 <p>
                     Nome Completo: <span>(Campo Obrigatório)</span>
                 </p>
-                <input 
-                    type="text" 
-                    name='nome' 
-                    id='nome' 
+                <input
+                    type="text"
+                    name='nome'
+                    id='nome'
                     placeholder='Digite o Nome Completo'
-                    onChange={getDadosForm}
-                    value={obj.nome} 
+                    onChange={handleInputChange}
+                    value={obj.nome}
                     required
                 />
             </NomeCompleto>
@@ -22,13 +72,13 @@ function FormUsuario({ getDadosForm, cadastrar, obj }){
                 <p>
                     E-mail: <span>(Campo Obrigatório)</span>
                 </p>
-                <input 
-                    type="text" 
-                    name='email' 
-                    id='email' 
+                <input
+                    type="email"
+                    name='email'
+                    id='email'
                     placeholder='Digite o e-mail'
-                    onChange={getDadosForm}
-                    value={obj.email}  
+                    onChange={handleInputChange}
+                    value={obj.email}
                     required
                 />
             </EmailUser>
@@ -37,21 +87,26 @@ function FormUsuario({ getDadosForm, cadastrar, obj }){
                 <p>
                     Senha: <span>(Campo Obrigatório)</span>
                 </p>
-                <input 
-                    type="text" 
-                    name='senha' 
-                    id='senha' 
+                <input
+                    type="text"
+                    name='senha'
+                    id='senha'
                     placeholder='Digite a senha'
-                    onChange={getDadosForm}
-                    value={obj.senha}  
+                    onChange={handleInputChange}
+                    value={obj.senha}
                     required
                 />
             </SenhaUser>
-             
-
-            <button onClick={cadastrar} type='submit'>
-                Salvar
-            </button>
+            
+            {!isFormValid ? (
+                <ButtonBlock disabled={!isFormValid}>
+                    X Salvar
+                </ButtonBlock>
+            ) : (
+                <button onClick={handleSubmit} type='submit'>
+                    Salvar
+                </button>
+            )}
         </Cmain>
     );
 }
