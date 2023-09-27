6dc5662c0ec74f2ce8ac834b1beafef46f1aa852
André Nunes <andre.doctors@gmail.com>
2023-09-27 04:39:59 -0300

    Feat: Backend Adicione a saida de estoque de municao por avaria

--- backend/municao/src/main/java/br/municao/controllers/SaidaMunicaoController.java
+++ backend/municao/src/main/java/br/municao/controllers/SaidaMunicaoController.java
@@ -1,16 +1,32 @@
 package br.municao.controllers;
 
+import br.municao.models.EntradaMunicaoModel;
+import br.municao.models.SaidaMunicaoModel;
 import br.municao.services.SaidaMunicaoService;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
-@RequestMapping("/saida-municao")
+@RequestMapping("/estoquesaida")
 public class SaidaMunicaoController {
 
     private final SaidaMunicaoService saidaMunicaoService;
 
     public SaidaMunicaoController(SaidaMunicaoService saidaMunicaoService){
         this.saidaMunicaoService = saidaMunicaoService;
+    }
+
+    @PostMapping("/saida")
+    public ResponseEntity<String> saidaEstoqueMunicao(@RequestBody SaidaMunicaoModel saida) {
+        try {
+            saidaMunicaoService.saidaEstoqueMunicao(saida);
+            return new ResponseEntity<>("Estoque de munição atualizado com sucesso", HttpStatus.OK);
+        } catch (Exception e) {
+            return new ResponseEntity<>("Erro ao atualizar o estoque de munição: " + e.getMessage(), HttpStatus.INTERNAL_SERVER_ERROR);
+        }
     }
 }
--- backend/municao/src/main/java/br/municao/services/SaidaMunicaoService.java
+++ backend/municao/src/main/java/br/municao/services/SaidaMunicaoService.java
@@ -1,5 +1,9 @@
 package br.municao.services;
 
+import br.municao.models.EntradaMunicaoModel;
+import br.municao.models.MunicaoModel;
+import br.municao.models.SaidaMunicaoModel;
+import br.municao.repositories.MunicaoRepository;
 import br.municao.repositories.SaidaMunicaoRepository;
 import br.municao.response.SmsResponse;
 import org.springframework.stereotype.Service;
@@ -8,10 +12,25 @@ import org.springframework.stereotype.Service;
 public class SaidaMunicaoService {
 
     private final SaidaMunicaoRepository saidaMunicaoRepository;
+    private final MunicaoRepository municaoRepository;
     private final SmsResponse smsResponse;
 
-    public SaidaMunicaoService(SaidaMunicaoRepository saidaMunicaoRepository){
+    public SaidaMunicaoService(SaidaMunicaoRepository saidaMunicaoRepository, MunicaoRepository municaoRepository){
         this.saidaMunicaoRepository = saidaMunicaoRepository;
+        this.municaoRepository = municaoRepository;
         this.smsResponse = new SmsResponse();
+    }
+
+    public void saidaEstoqueMunicao(SaidaMunicaoModel newSaida){
+        // Supondo que você tenha um critério para identificar a munição, como um ID único.
+        MunicaoModel municao = municaoRepository.findById(newSaida.getMunicao().getId()).orElse(new MunicaoModel()); // Obtém o objeto MunicaoModel existente ou cria um novo.
+
+        long quantidadeAtual = municao.getQuantidade();
+        long quantidadeNova = newSaida.getQuantidade();
+
+        long total = quantidadeAtual - quantidadeNova;
+
+        municao.setQuantidade(total);
+        saidaMunicaoRepository.save(newSaida);
     }
 }
