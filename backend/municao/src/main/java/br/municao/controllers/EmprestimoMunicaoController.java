bcf06ae2b9541025018db8f4b98e4eaf4f0a266e
André Nunes <andre.doctors@gmail.com>
2023-09-27 04:38:56 -0300

    Feat: Backend adicionei a funcao de Emprestimo de municao

--- backend/municao/src/main/java/br/municao/controllers/EmprestimoMunicaoController.java
+++ backend/municao/src/main/java/br/municao/controllers/EmprestimoMunicaoController.java
@@ -1,6 +1,12 @@
 package br.municao.controllers;
 
+import br.municao.models.EmprestimoMunicaoModel;
+import br.municao.models.EntradaMunicaoModel;
 import br.municao.services.EmprestimoMunicaoService;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
@@ -13,4 +19,15 @@ public class EmprestimoMunicaoController {
     public EmprestimoMunicaoController(EmprestimoMunicaoService emprestimoMunicaoService){
         this.emprestimoMunicaoService = emprestimoMunicaoService;
     }
+
+    @PostMapping("/emprestar")
+    public ResponseEntity<String> entradaEstoqueMunicao(@RequestBody EmprestimoMunicaoModel emprestar) {
+        try {
+            emprestimoMunicaoService.emprestar(emprestar);
+            return new ResponseEntity<>("Estoque de munição atualizado com sucesso", HttpStatus.OK);
+        } catch (Exception e) {
+            return new ResponseEntity<>("Erro ao atualizar o estoque de munição: " + e.getMessage(), HttpStatus.INTERNAL_SERVER_ERROR);
+        }
+    }
+
 }
--- backend/municao/src/main/java/br/municao/services/EmprestimoMunicaoService.java
+++ backend/municao/src/main/java/br/municao/services/EmprestimoMunicaoService.java
@@ -1,6 +1,10 @@
 package br.municao.services;
 
+import br.municao.models.EmprestimoMunicaoModel;
+import br.municao.models.EntradaMunicaoModel;
+import br.municao.models.MunicaoModel;
 import br.municao.repositories.EmprestimoMunicaoRepository;
+import br.municao.repositories.MunicaoRepository;
 import br.municao.response.SmsResponse;
 import org.springframework.stereotype.Service;
 
@@ -9,10 +13,26 @@ public class EmprestimoMunicaoService {
 
     private final EmprestimoMunicaoRepository emprestimoMunicaoRepository;
 
+    private final MunicaoRepository municaoRepository;
+
     private final SmsResponse smsResponse;
 
-    public EmprestimoMunicaoService(EmprestimoMunicaoRepository emprestimoMunicaoRepository){
+    public EmprestimoMunicaoService(EmprestimoMunicaoRepository emprestimoMunicaoRepository, MunicaoRepository municaoRepository){
         this.emprestimoMunicaoRepository = emprestimoMunicaoRepository;
+        this.municaoRepository = municaoRepository;
         this.smsResponse = new SmsResponse();
+    }
+
+    public void emprestar(EmprestimoMunicaoModel emprestimo){
+        // Supondo que você tenha um critério para identificar a munição, como um ID único.
+        MunicaoModel municao = municaoRepository.findById(emprestimo.getMunicao().getId()).orElse(new MunicaoModel()); // Obtém o objeto MunicaoModel existente ou cria um novo.
+
+        long quantidadeAtual = municao.getQuantidade();
+        long quantidadeNova = emprestimo.getQuantidade();
+
+        long total = quantidadeAtual - quantidadeNova;
+
+        municao.setQuantidade(total);
+        emprestimoMunicaoRepository.save(emprestimo);
     }
 }
