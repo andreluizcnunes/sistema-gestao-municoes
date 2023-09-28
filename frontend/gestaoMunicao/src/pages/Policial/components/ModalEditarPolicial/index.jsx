ae2946f8bd876c97536949ddfc9a58d982ce7ded
André Nunes <andre.doctors@gmail.com>
2023-09-27 22:49:26 -0300

    Refatoração do Modal de Edição de Policial
    Nesta alteração, refatorei o Modal de Edição de Policial para adicionar validações de entrada em tempo real nos campos Nome, CPF, RG, Data de Nascimento e Telefone. Agora, os campos são validados ao perderem o foco, exibindo mensagens de erro se necessário. Além disso, o botão "Salvar" só fica habilitado se houver alterações nos campos. Isso melhora a experiência do usuário ao editar informações de um Policial.

--- frontend/gestaoMunicao/src/pages/Policial/components/ModalEditarPolicial/index.jsx
+++ frontend/gestaoMunicao/src/pages/Policial/components/ModalEditarPolicial/index.jsx
@@ -1,11 +1,13 @@
-import React, { useState } from 'react';
+import React, { useEffect, useState } from 'react';
 
-import { 
-    ModalWrapper, 
-    ModalContent, 
-    ContainerInput, 
-    ContainerButton, 
-    PolicialCpf, 
+import InputMask from 'react-input-mask';
+
+import {
+    ModalWrapper,
+    ModalContent,
+    ContainerInput,
+    ContainerButton,
+    PolicialCpf,
     PolicialRg,
     PolicialDataNascimento,
     PolicialTelefone
@@ -14,13 +16,152 @@ import {
 function ModalEditarPolicial({ policial, onClose, onSave }) {
 
     const [editedPolicial, setEditedPolicial] = useState(policial);
+    const [isDirty, setIsDirty] = useState(false); // Estado para rastrear alterações
+
+    // Estados para rastrear erros de validação
+    const [validationErrors, setValidationErrors] = useState({
+        nome: '',
+        cpf: '',
+        rg: '',
+        dataNascimento: '',
+        telefone: ''
+    });
+
+    useEffect(() => {
+        // Verifique se o objeto 'editedUsuario' é diferente do objeto 'usuario' original
+        const isEdited = JSON.stringify(editedPolicial) !== JSON.stringify(policial);
+        setIsDirty(isEdited);
+    }, [editedPolicial, policial]);
 
     const handleInputChange = (e) => {
         const { name, value } = e.target;
-        setEditedPolicial({ ...editedPolicial, [name]: value });
+        // setEditedPolicial({ ...editedPolicial, [name]: value });
+        setEditedPolicial((prevEditedPolicial) => ({
+            ...prevEditedPolicial,
+            [name]: value,
+        }));
     };
 
+    // Função de validação para o campo "Nome do Policial"
+    const validateNome = () => {
+        if (!editedPolicial.nome) {
+            setValidationErrors((prevErrors) => ({
+                ...prevErrors,
+                nome: 'O nome do Policial é obrigatório.',
+            }));
+        } else {
+            setValidationErrors((prevErrors) => ({
+                ...prevErrors,
+                nome: '',
+            }));
+        }
+    };
+
+    // Função de validação para o campo "CPF"
+    const validateCpf = () => {
+        if (!editedPolicial.cpf) {
+            setValidationErrors((prevErrors) => ({
+                ...prevErrors,
+                cpf: 'O cpf do Policial é obrigatório.',
+            }));
+        } else {
+            setValidationErrors((prevErrors) => ({
+                ...prevErrors,
+                cpf: '',
+            }));
+        }
+    };
+
+    // Função de validação para o campo "RG"
+    const validateRg = () => {
+        if (!editedPolicial.rg) {
+            setValidationErrors((prevErrors) => ({
+                ...prevErrors,
+                rg: 'O RG do Policial é obrigatório.',
+            }));
+        } else {
+            setValidationErrors((prevErrors) => ({
+                ...prevErrors,
+                rg: '',
+            }));
+        }
+    };
+
+    // Função de validação para o campo "Data Nascimento"
+    const validateDataNascimento = () => {
+        if (!editedPolicial.dataNascimento) {
+            setValidationErrors((prevErrors) => ({
+                ...prevErrors,
+                dataNascimento: 'O Data Nascimento do Policial é obrigatório.',
+            }));
+        } else {
+            setValidationErrors((prevErrors) => ({
+                ...prevErrors,
+                dataNascimento: '',
+            }));
+        }
+    };
+
+    // Função de validação para o campo "Data Nascimento"
+    const validateTelefone = () => {
+        if (!editedPolicial.telefone) {
+            setValidationErrors((prevErrors) => ({
+                ...prevErrors,
+                telefone: 'O Telefone do Policial é obrigatório.',
+            }));
+        } else {
+            setValidationErrors((prevErrors) => ({
+                ...prevErrors,
+                telefone: '',
+            }));
+        }
+    };
+
+    // const handleSave = async () => {
+    //     try {
+    //         // Realize a requisição PUT para a rota da API para editar o Policial
+    //         const response = await fetch('http://localhost:8080/policial/editar', {
+    //             method: 'PUT',
+    //             headers: {
+    //                 'Content-Type': 'application/json',
+    //             },
+    //             body: JSON.stringify(editedPolicial),
+    //         });
+
+    //         if (response.ok) {
+    //             // Se a requisição for bem-sucedida, chame a função onSave para atualizar o marca no estado pai
+    //             onSave(editedPolicial);
+    //             onClose();
+    //             window.location.reload();
+    //         } else {
+    //             // Trate os erros da API, se necessário
+    //             console.error('Erro ao editar Policial');
+    //         }
+    //     } catch (error) {
+    //         // Trate erros de rede ou outros erros aqui
+    //         console.error('Erro ao editar Policial:', error);
+    //     }
+    // };
+
     const handleSave = async () => {
+        // Validar os campos antes de tentar salvar
+        validateNome();
+        validateCpf();
+        validateRg();
+        validateDataNascimento();
+        validateTelefone();
+
+        // Verificar se há erros de validação
+        if (
+            validationErrors.nome ||
+            validationErrors.cpf ||
+            validationErrors.rg ||
+            validationErrors.dataNascimento ||
+            validationErrors.telefone
+        ) {
+            return; // Impedir que o salvamento continue se houver erros
+        }
+
         try {
             // Realize a requisição PUT para a rota da API para editar o Policial
             const response = await fetch('http://localhost:8080/policial/editar', {
@@ -56,16 +197,29 @@ function ModalEditarPolicial({ policial, onClose, onSave }) {
                     name="nome"
                     value={editedPolicial.nome}
                     onChange={handleInputChange}
+                    onBlur={validateNome} // Validar ao perder o foco
                 />
                 <ContainerInput>
                     <PolicialCpf>
                         CPF:
 
-                        <input
+                        {/* <input
                             type="text"
-                            name="nome"
+                            name="cpf"
                             value={editedPolicial.cpf}
                             onChange={handleInputChange}
+                            onBlur={validateCpf} // Validar ao perder o foco
+                        /> */}
+
+                        <InputMask
+                            mask="999.999.999-99" // Defina a máscara do CPF
+                            type="text"
+                            name="cpf"
+                            id="cpf"
+                            placeholder="Digite o CPF"
+                            onChange={handleInputChange}
+                            value={editedPolicial.cpf}
+                            onBlur={validateCpf} // Validar ao perder o foco
                         />
 
                     </PolicialCpf>
@@ -74,9 +228,10 @@ function ModalEditarPolicial({ policial, onClose, onSave }) {
                         RG:
                         <input
                             type="text"
-                            name="nome"
+                            name="rg"
                             value={editedPolicial.rg}
                             onChange={handleInputChange}
+                            onBlur={validateRg} // Validar ao perder o foco
                         />
                     </PolicialRg>
 
@@ -88,9 +243,10 @@ function ModalEditarPolicial({ policial, onClose, onSave }) {
                         Data Nascimento:
                         <input
                             type="date"
-                            name="nome"
+                            name="dataNascimento"
                             value={editedPolicial.dataNascimento}
                             onChange={handleInputChange}
+                            onBlur={validateDataNascimento} // Validar ao perder o foco
                         />
                     </PolicialDataNascimento>
 
@@ -98,16 +254,17 @@ function ModalEditarPolicial({ policial, onClose, onSave }) {
                         Telefone:
                         <input
                             type="text"
-                            name="nome"
+                            name="telefone"
                             value={editedPolicial.telefone}
                             onChange={handleInputChange}
+                            onBlur={validateTelefone} // Validar ao perder o foco
                         />
                     </PolicialTelefone>
 
                 </ContainerInput>
 
                 <ContainerButton>
-                    <button onClick={handleSave}>Salvar</button>
+                    <button onClick={handleSave} disabled={!isDirty}>Salvar</button>
                     <button onClick={onClose}>Cancelar</button>
                 </ContainerButton>
             </ModalContent>
