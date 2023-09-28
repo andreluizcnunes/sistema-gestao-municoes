4f007d3f2b09d9704dd480de92f2d1ec2e55e94b
André Nunes <andre.doctors@gmail.com>
2023-09-27 23:48:59 -0300

    remove: removi os icones do botao submenu

--- frontend/gestaoMunicao/src/pages/Policial/components/HeaderPolicial/index.jsx
+++ frontend/gestaoMunicao/src/pages/Policial/components/HeaderPolicial/index.jsx
@@ -2,9 +2,6 @@ import { ContentInfo, ContainerBtn } from "./HeaderPolicial.styled"
 
 import BtnMenu from "../../../../components/BtnMenu";
 
-import IconAdd from "../../../../assets/icons/iconAdd.png";
-import IconBusca from "../../../../assets/icons/iconBusca.png";
-import IconHome from "../../../../assets/icons/iconHome.png";
 
 function HeaderPolicial({ titulo }){
     return(
@@ -12,9 +9,9 @@ function HeaderPolicial({ titulo }){
             <h2>{titulo}</h2>
             <hr />
             <ContainerBtn>
-                <BtnMenu linkBotao={"/policial"} iconBotao={IconHome} textBotao={"Inicio"} />
-                <BtnMenu linkBotao={"/policial/cadastro"} iconBotao={IconAdd} textBotao={"Cadastrar"} />
-                <BtnMenu linkBotao={"/policial/listar"} iconBotao={IconBusca} textBotao={"Consultar"} />
+                <BtnMenu linkBotao={"/policial"} textBotao={"Inicio"} />
+                <BtnMenu linkBotao={"/policial/cadastro"}  textBotao={"Cadastrar"} />
+                <BtnMenu linkBotao={"/policial/listar"} textBotao={"Consultar"} />
             </ContainerBtn>
 
         </ContentInfo>
--- frontend/gestaoMunicao/src/pages/Usuario/components/HeaderUser/index.jsx
+++ frontend/gestaoMunicao/src/pages/Usuario/components/HeaderUser/index.jsx
@@ -2,8 +2,6 @@ import { ContentInfo, ContainerBtn } from "./HeaderUser.styled"
 
 import BtnMenu from "../../../../components/BtnMenu";
 
-import IconAdd from "../../../../assets/icons/iconAdd.png";
-import IconBusca from "../../../../assets/icons/iconBusca.png";
 
 function HeaderUser({ titulo }){
     return(
@@ -12,8 +10,8 @@ function HeaderUser({ titulo }){
             <hr />
             <ContainerBtn>
                 <BtnMenu linkBotao={"/usuario"} textBotao={"Lista"} />
-                <BtnMenu linkBotao={"/usuario/cadastro"} iconBotao={IconAdd} textBotao={"Cadastrar"} />
-                <BtnMenu linkBotao={"/usuario/listar"} iconBotao={IconBusca} textBotao={"Consultar"} />
+                <BtnMenu linkBotao={"/usuario/cadastro"} textBotao={"Cadastrar"} />
+                <BtnMenu linkBotao={"/usuario/listar"} textBotao={"Consultar"} />
             </ContainerBtn>
 
         </ContentInfo>
