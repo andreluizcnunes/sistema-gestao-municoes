02d3929a778f3a70c9dab0324154174b0a362115
André Nunes <andre.doctors@gmail.com>
2023-09-28 11:12:19 -0300

    feat: Frontend Adicione validação de campos e lógica de salvamento no ModalEditarFornecedor
    - Adicionei estados para rastrear alterações e erros de validação.
    - Implementei a validação para os campos Razão Social, CNPJ e Inscrição.
    - Bloqueei o botão "Salvar" quando há erros de validação ou nenhum campo foi modificado.
    - A lógica de salvamento agora verifica a validação antes de fazer a requisição PUT.
    - Corrigi a definição de erros nos campos de validação.
    - Atualizei o componente para exibir mensagens de erro ao lado dos campos.
    - Realizo um reload da página após o salvamento bem-sucedido para atualizar os dados.

--- frontend/gestaoMunicao/src/pages/Fornecedor/components/ModalEditarFornecedor/index.jsx
+++ frontend/gestaoMunicao/src/pages/Fornecedor/components/ModalEditarFornecedor/index.jsx
@@ -1,17 +1,88 @@
-import React, { useState } from 'react';
+import React, { useEffect, useState } from 'react';
 
 import { ModalWrapper, ModalContent, ContainerButton } from "./ModalEditarFornecedor.styled"
 
 function ModalEditarFornecedor({ fornecedor, onClose, onSave }) {
 
     const [editedFornecedor, setEditedFornecedor] = useState(fornecedor);
+    const [isDirty, setIsDirty] = useState(false); // Estado para rastrear alterações
+
+    const [validationErrors, setValidationErrors] = useState({
+        razaoSocial: '',
+        cnpj: '',
+        inscricao: ''
+    });
+
+
+    useEffect(() => {
+        // Verifique se o objeto 'editedUsuario' é diferente do objeto 'usuario' original
+        const isEdited = JSON.stringify(editedFornecedor) !== JSON.stringify(fornecedor);
+        setIsDirty(isEdited);
+    }, [editedFornecedor, fornecedor]);
 
     const handleInputChange = (e) => {
         const { name, value } = e.target;
-        setEditedFornecedor({ ...editedFornecedor, [name]: value });
+        setEditedFornecedor((prevEditedFornecedor) => ({
+            ...prevEditedFornecedor,
+            [name]: value,
+        }));
+    };
+
+    const validateRazaoSocial = () => {
+        if (!editedFornecedor.razaoSocial) {
+            setValidationErrors((prevErrors) => ({
+                ...prevErrors,
+                razaoSocial: 'O Razão Social é obrigatório.',
+            }));
+        } else {
+            setValidationErrors((prevErrors) => ({
+                ...prevErrors,
+                razaoSocial: '',
+            }));
+        }
+    };
+
+    const validateCnpj = () => {
+        if (!editedFornecedor.cnpj) {
+            setValidationErrors((prevErrors) => ({
+                ...prevErrors,
+                cnpj: 'O CNPJ é obrigatório.',
+            }));
+        } else {
+            setValidationErrors((prevErrors) => ({
+                ...prevErrors,
+                cnpj: '',
+            }));
+        }
+    };
+
+    const validateInscricao = () => {
+        if (!editedFornecedor.inscricao) {
+            setValidationErrors((prevErrors) => ({
+                ...prevErrors,
+                inscricao: 'A Inscrição é obrigatório.',
+            }));
+        } else {
+            setValidationErrors((prevErrors) => ({
+                ...prevErrors,
+                inscricao: '',
+            }));
+        }
     };
 
     const handleSave = async () => {
+        // Validar os campos antes de tentar salvar
+        validateRazaoSocial();
+        validateCnpj();
+        validateInscricao();
+
+        // Verificar se há erros de validação
+        if (
+            validationErrors.nome
+        ) {
+            return; // Impedir que o salvamento continue se houver erros
+        }
+
         try {
             // Realize a requisição PUT para a rota da API para editar o fornecedor
             const response = await fetch('http://localhost:8080/fornecedor/editar', {
@@ -23,17 +94,17 @@ function ModalEditarFornecedor({ fornecedor, onClose, onSave }) {
             });
 
             if (response.ok) {
-                // Se a requisição for bem-sucedida, chame a função onSave para atualizar o fornecedor no estado pai
+                // Se a requisição for bem-sucedida, chame a função onSave para atualizar o marca no estado pai
                 onSave(editedFornecedor);
                 onClose();
                 window.location.reload();
             } else {
                 // Trate os erros da API, se necessário
-                console.error('Erro ao editar fornecedor');
+                console.error('Erro ao editar Fornecedor');
             }
         } catch (error) {
             // Trate erros de rede ou outros erros aqui
-            console.error('Erro ao editar fornecedor:', error);
+            console.error('Erro ao editar Fornecedor:', error);
         }
     };
 
@@ -47,23 +118,26 @@ function ModalEditarFornecedor({ fornecedor, onClose, onSave }) {
                     name="razaoSocial"
                     value={editedFornecedor.razaoSocial}
                     onChange={handleInputChange}
+                    onBlur={validateRazaoSocial}
                 />
                 <label>CNPJ:</label>
                 <input
-                    type="text"
+                    type="number"
                     name="cnpj"
                     value={editedFornecedor.cnpj}
                     onChange={handleInputChange}
+                    onBlur={validateCnpj}
                 />
                 <label>Inscrição:</label>
                 <input
-                    type="text"
+                    type="number"
                     name="inscricao"
                     value={editedFornecedor.inscricao}
                     onChange={handleInputChange}
+                    onBlur={validateInscricao}
                 />
                 <ContainerButton>
-                    <button onClick={handleSave}>Salvar</button>
+                    <button onClick={handleSave} disabled={!isDirty}>Salvar</button>
                     <button onClick={onClose}>Cancelar</button>
                 </ContainerButton>
             </ModalContent>
