a597d8ca13bd71746556d698341003fcc625a740
André Nunes <andre.doctors@gmail.com>
2023-09-28 01:04:36 -0300

    Feat: Frontend Refatora FormMarca para adicionar validação de campos e atualização condicional do botão de salvar

--- frontend/gestaoMunicao/src/pages/Marca/components/FormMarca/index.jsx
+++ frontend/gestaoMunicao/src/pages/Marca/components/FormMarca/index.jsx
@@ -1,6 +1,35 @@
-import { Cmain, NomeMarca } from './FormMarca.styled';
+import { useState } from 'react';
+
+import { Cmain, NomeMarca, ButtonBlock } from './FormMarca.styled';
 
 function FormMarca({ getDadosForm, cadastrar, obj }){
+
+    const [isFormValid, setIsFormValid] = useState(false);
+
+    const handleSubmit = (e) => {
+        e.preventDefault(); // Evite a submissão padrão do formulário
+        if (isFormValid) {
+            cadastrar(); // Chame a função de cadastro;
+            window.location.reload();
+        }
+    };
+
+    const handleInputChange = (e) => {
+        getDadosForm(e);
+
+        // Valide os campos aqui e atualize o estado isFormValid
+        const { name, value } = e.target;
+        const updatedObj = {
+            ...obj,
+            [name]: value
+        };
+
+        const areFieldsFilled =
+            updatedObj.nome.trim() !== '';
+
+        setIsFormValid(areFieldsFilled);
+    };
+
     return(
         <Cmain>
             <NomeMarca htmlFor="nomeMarca">
@@ -12,15 +41,21 @@ function FormMarca({ getDadosForm, cadastrar, obj }){
                     name='nome' 
                     id='nomeMarca' 
                     placeholder='Digite o Nome da Marca'
-                    onChange={getDadosForm}
+                    onChange={handleInputChange}
                     value={obj.nome}
                     required
                 />
             </NomeMarca>           
 
-            <button onClick={cadastrar} type='submit'>
-                Salvar
-            </button>
+            {!isFormValid ? (
+                <ButtonBlock disabled={!isFormValid}>
+                    X Salvar
+                </ButtonBlock>
+            ) : (
+                <button onClick={handleSubmit} type='submit'>
+                    Salvar
+                </button>
+            )}
         </Cmain>
     );
 }
