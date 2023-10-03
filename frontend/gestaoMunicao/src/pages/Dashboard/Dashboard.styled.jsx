eef71f04f5f6e605f332be88b6e5593a0c85f22a
André Nunes <andre.doctors@gmail.com>
2023-10-03 15:29:49 -0300

    Styled: Frontend estilização da Dashboard

--- frontend/gestaoMunicao/src/pages/Dashboard/Dashboard.styled.jsx
+++ frontend/gestaoMunicao/src/pages/Dashboard/Dashboard.styled.jsx
@@ -1,5 +1,18 @@
 import styled from 'styled-components'
 
 export const CMain = styled.div`
-    
+
+`;
+
+export const ContentMain = styled.div`
+    display: flex;
+    margin-left: 15%;
+`;
+export const CSection = styled.section`
+    width: 100%;
+    margin: 7% auto 0 auto;
+    display: flex;
+    flex-wrap: wrap;
+    justify-content: space-around;
+    gap: 1rem;
 `;
