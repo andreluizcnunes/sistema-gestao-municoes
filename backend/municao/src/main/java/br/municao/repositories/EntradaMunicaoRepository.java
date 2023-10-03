873ca0cd2fa0261a61f4e907991bda729d71d329
André Nunes <andre.doctors@gmail.com>
2023-10-03 15:02:07 -0300

    Add: Backend Adicionei consulta de contagem total de itens no EntradaMunicaoRepository
    Neste commit, adicionei uma consulta personalizada no repositório EntradaMunicaoRepository para contar o número total de itens na entidade EntradaMunicaoModel. A consulta utiliza a anotação @Query para definir a consulta SQL personalizada que realiza a contagem.

--- backend/municao/src/main/java/br/municao/repositories/EntradaMunicaoRepository.java
+++ backend/municao/src/main/java/br/municao/repositories/EntradaMunicaoRepository.java
@@ -2,6 +2,9 @@ package br.municao.repositories;
 
 import br.municao.models.EntradaMunicaoModel;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Query;
 
 public interface EntradaMunicaoRepository extends JpaRepository<EntradaMunicaoModel, Long> {
+    @Query("SELECT COUNT(e) FROM EntradaMunicaoModel e")
+    long countTotalItems();
 }
