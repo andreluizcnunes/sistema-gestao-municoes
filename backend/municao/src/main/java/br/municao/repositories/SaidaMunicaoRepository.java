72516247c1072651b533a2f79e121a39cd66c82c
André Nunes <andre.doctors@gmail.com>
2023-10-03 15:10:13 -0300

    Add: Backend Adicionei consulta de contagem total de itens no SaidaMunicaoRepository
    Neste commit, adicionei uma consulta personalizada no repositório SaidaMunicaoRepository para contar o número total de itens na entidade SaidaMunicaoModel. A consulta utiliza a anotação @Query para definir a consulta SQL personalizada que realiza a contagem.

--- backend/municao/src/main/java/br/municao/repositories/SaidaMunicaoRepository.java
+++ backend/municao/src/main/java/br/municao/repositories/SaidaMunicaoRepository.java
@@ -2,6 +2,10 @@ package br.municao.repositories;
 
 import br.municao.models.SaidaMunicaoModel;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Query;
 
 public interface SaidaMunicaoRepository extends JpaRepository<SaidaMunicaoModel, Long> {
+
+    @Query("SELECT COUNT(s) FROM SaidaMunicaoModel s")
+    long countTotalItems();
 }
