cf88a396abf8ec68b545683959386829b87e4d85
André Nunes <andre.doctors@gmail.com>
2023-10-03 15:07:30 -0300

    Add: Backend Adicionei consultas personalizadas ao MunicaoRepository
    Neste commit, implementei consultas personalizadas no repositório MunicaoRepository para buscar munições com base em critérios específicos:
    - `findByNomeMunicaoDto` para buscar munições por nome (ignorando maiúsculas/minúsculas).
    - `countTotalItems` para contar o número total de munições.
    - `countMunicoesComQuantidadeMenorQue1000` para contar munições com quantidade inferior a 1000.
    - `countMunicoesComQuantidadeMenorQue3000` para contar munições com quantidade entre 1000 e 3000.
    
    Essas consultas personalizadas facilitarão a recuperação de dados relacionados às munições em diferentes situações.

--- backend/municao/src/main/java/br/municao/repositories/MunicaoRepository.java
+++ backend/municao/src/main/java/br/municao/repositories/MunicaoRepository.java
@@ -1,6 +1,5 @@
 package br.municao.repositories;
 
-import br.municao.dto.MarcaDTO;
 import br.municao.dto.MunicaoDTO;
 import br.municao.models.MunicaoModel;
 import org.springframework.data.jpa.repository.JpaRepository;
@@ -10,6 +9,15 @@ import java.util.List;
 
 public interface MunicaoRepository extends JpaRepository<MunicaoModel, Long> {
 
-    @Query("select m from MunicaoModel m where m.nome like %?1%")
+    @Query("select m from MunicaoModel m where lower(m.nome) like lower(concat('%', ?1, '%'))")
     List<MunicaoDTO> findByNomeMunicaoDto(String nome);
+
+    @Query("SELECT COUNT(m) FROM MunicaoModel m")
+    long countTotalItems();
+
+    @Query("SELECT COUNT(m) FROM MunicaoModel m WHERE m.quantidade < 1000")
+    long countMunicoesComQuantidadeMenorQue1000();
+
+    @Query("SELECT COUNT(m) FROM MunicaoModel m WHERE m.quantidade > 1000 AND m.quantidade < 3000")
+    long countMunicoesComQuantidadeMenorQue3000();
 }
